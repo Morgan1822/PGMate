17c6b1130d31bddda9b36ae18595a4f4180238dd
vinoth <vinothvivekanandan22@gmail.com>
2026-06-25 05:13:43 +0530

    Initial Commit

---
+++ PGMate/PGMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGMateApp.swift
+//  PGMate
+//
+//  Created by Vinoth on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PGMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
